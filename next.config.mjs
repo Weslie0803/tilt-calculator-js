50ca7b0314cbdb38c307449b8d40603682c5ddc3
weslie0803 <weslie0803@126.com>
2024-01-28 02:22:29 +0800

    fix: set basePath to fit github

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    basePath: "/tilt_calculator-js",
+    assetPrefix: "/tile_calculator-js/"
 };
 
 export default nextConfig;
